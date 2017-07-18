f57c5980fad89525c964c2e9f394868c356ee2b8
dhrmnfce <dylanludwick@gmail.com>
2017-07-18 23:46:40 +0000

    Create static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,7 @@
 class StaticPagesController < ApplicationController
   
   def home
+    @meta_description = "TenantNest is an online solution for money transfers, service and maintenance inquiries/task management, and SEO friendly vacancy listings."
   end
 
   def about
